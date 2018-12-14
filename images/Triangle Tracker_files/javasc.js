beaf2ec876c2062868adb104f4ada566c9c72260
UWABIKIRAMARIYA Roselyne <mariadelarosita078@gmail.com>
2018-12-14 15:38:05 +0200

    Add more in a triangle tracker project

---
+++ images/Triangle Tracker_files/javasc.js
@@ -0,0 +1,7 @@
+function getResult(){
+    var number1= parseFloat(document.getElementById("nbr1").value);
+    var number2= parseFloat(document.getElementById("nbr2").value);
+    var number3= parseFloat(document.getElementById("nbr3").value);
+}
+
+
